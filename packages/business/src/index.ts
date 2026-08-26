b2b168f7d9c29587fd89f7d88b8b453b4d97c032
yurian51 <yurianarshad@gmail.com>
2026-08-26 17:46:18 +0300

    feat(business): export inventory and payment services

--- packages/business/src/index.ts
+++ packages/business/src/index.ts
@@ -2,4 +2,6 @@ export * from "./catalog.service";
 export * from "./customer.service";
 export * from "./order.service";
 export * from "./order-create.service";
+export * from "./inventory.service";
+export * from "./payment-intent.service";
 export * from "./support.service";
