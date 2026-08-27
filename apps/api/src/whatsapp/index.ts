f164bb3ad5850741b7e8c5ecd351af7e83f86afa
yurian51 <yurianarshad@gmail.com>
2026-08-27 05:53:10 +0300

    feat(whatsapp): export ingress pipeline

--- apps/api/src/whatsapp/index.ts
+++ apps/api/src/whatsapp/index.ts
@@ -1,2 +1,7 @@
 export * from "./events";
 export * from "./webhook";
+export * from "./normalize";
+export * from "./message-router";
+export * from "./event-id";
+export * from "./queue-dispatch";
+export * from "./ingress.service";
