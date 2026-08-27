5772d24b8375831a09205a9a3b655bf1a4180863
yurian51 <yurianarshad@gmail.com>
2026-08-27 07:57:28 +0300

    feat(commerce): export governed commerce services

--- packages/commerce/src/index.ts
+++ packages/commerce/src/index.ts
@@ -3,3 +3,6 @@ export * from "./services";
 export * from "./order-state-machine";
 export * from "./payment-state-machine";
 export * from "./inventory-reservation";
+export * from "./order-service";
+export * from "./payment-service";
+export * from "./inventory-reservation-service";
