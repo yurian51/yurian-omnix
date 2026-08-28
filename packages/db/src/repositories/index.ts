4c1083f6a787bf45ed09a78b853f0830852175d4
yurian51 <yurianarshad@gmail.com>
2026-08-28 08:58:04 +0300

    feat(db): export payment and inventory repositories

--- packages/db/src/repositories/index.ts
+++ packages/db/src/repositories/index.ts
@@ -1,3 +1,5 @@
 export * from "./customer-repository";
 export * from "./product-repository";
 export * from "./order-repository";
+export * from "./payment-repository";
+export * from "./inventory-reservation-repository";
